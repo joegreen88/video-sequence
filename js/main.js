0c8e85b9b3e5e8e9570f505697260831764a3999
Joe Green <joe.green@smrtr.co.uk>
2014-01-04 16:04:05 +0000

    little bit of queueing and processing

--- js/main.js
+++ js/main.js
@@ -1,10 +1,48 @@
 $(document).ready(function(){
 
+    var $video = $('#video');
     var aspectRatio = {X: 16, Y: 9};
-    videoSequence.resizeVideo($('#video'), aspectRatio.X, aspectRatio.Y);
+
+    videoSequence.resizeVideo($video, aspectRatio.X, aspectRatio.Y);
 
     $(window).resize(function(){
-        videoSequence.resizeVideo($('#video'), aspectRatio.X, aspectRatio.Y);
+        videoSequence.resizeVideo($video, aspectRatio.X, aspectRatio.Y);
     });
+
+    var scenes = [
+        [
+            'MVI_9027',
+            'MVI_9028',
+            'MVI_9029'
+        ],
+        [
+            'MVI_9038',
+            'MVI_9041',
+            'MVI_9043'
+        ],
+        [
+            'MVI_9044',
+            'MVI_9047',
+            'MVI_9054'
+        ]
+    ];
+
+    var chains = [
+        null,
+        null,
+        [
+            [0, 0],
+            [0, 1],
+            [0, 2]
+        ]
+    ];
+
+    $video.on('ended', videoSequence.next($video, scenes, chains));
+
+    videoSequence.queue.push('playRandomVideo');
+    videoSequence.queue.push('playRandomVideo');
+    videoSequence.queue.push('playChainedVideo');
+
+    $video.play();
 
 });
--- js/video-sequence.js
+++ js/video-sequence.js
@@ -2,6 +2,95 @@ videoSequence = (function(){
 
     return {
 
+        queue: [],
+
+        sequence: [],
+
+        scene: 0,
+
+        playing: null,
+
+        /**
+         * @param $video  Required  JQuery video element
+         * @param scenes  Required
+         * @param chains  Optional  Array
+         *
+         * @returns {Function}
+         */
+        next: function($video, scenes) {
+            if (arguments.length > 2) {
+                var chains = arguments[2];
+            } else {
+                var chains = [];
+            }
+            return function() {
+                if (this.queue.length > 0) {
+                    var f = queue.shift();
+                    if ('playRandomVideo' == f) {
+                        videoSequence[f]($video, scenes, this.scene);
+                    }
+                    else if('playChainedVideo' == f) {
+                        videoSequence[f]($video, scenes, this.scene, chains);
+                    }
+                }
+            }
+        },
+
+        /**
+         * @param $video         Required  JQuery video element
+         * @param scenes         Required
+         * @param sceneCriteria  Optional  Int
+         */
+        playRandomVideo: function($video, scenes) {
+            if (arguments.length > 2) {
+                var sceneCriteria = parseInt(arguments[2]);
+            } else {
+                var sceneCriteria = this.getRandomInt(0, scenes.length);
+            }
+
+            if (sceneCriteria >= scenes.length) {
+                sceneCriteria = this.getRandomInt(0, scenes.length);
+            }
+
+            var videoCriteria = this.getRandomInt(0, scenes[sceneCriteria].length - 1);
+            this.switchVideo($video, scenes, sceneCriteria, videoCriteria);
+        },
+
+        /**
+         * @param $video         Required  JQuery video element
+         * @param scenes         Required
+         * @param sceneCriteria  Required  Int
+         * @param chains         Required  Array
+         */
+        playChainedVideo: function($video, scenes, sceneCriteria, chains) {
+            for (var i in chains[sceneCriteria]) {
+                var link = chains[sceneCriteria][i];
+                if (this.sequence[link[0]] == link) {
+                    this.switchVideo($video, scenes, sceneCriteria, i);
+                    break;
+                }
+                console.error('no matching video chain found');
+            }
+        },
+
+        /**
+         * @param $video         Required  JQuery video element
+         * @param scenes         Required
+         * @param sceneCriteria  Required  Int
+         * @param videoCriteria  Required  Int
+         */
+        switchVideo: function($video, scenes, sceneCriteria, videoCriteria) {
+            var basename = scenes[sceneCriteria][videoCriteria];
+            var mp4Filename = 'video/' + basename + '.mp4';
+            var webmFilename = ''; // todo
+            $video.find('#mp4-source').attr('src', mp4Filename);
+            $video.find('#webm-source').attr('src', webmFilename); // todo
+            $video.load();
+            this.sequence.push([sceneCriteria, videoCriteria]);
+            this.playing = [sceneCriteria, videoCriteria];
+            this.scene = this.scene + 1;
+        },
+
         /**
          * @param $video        Required  JQuery video element
          * @param aspectRatioX  Required  Int
@@ -18,41 +107,6 @@ videoSequence = (function(){
             width = width - aspectRatioX;
             height = height - aspectRatioY;
             $video.width(width).height(height);
-        },
-
-        /**
-         * @param $video         Required  JQuery video element
-         * @param scenes         Required
-         * @param sceneCriteria  Required  Int
-         * @param videoCriteria  Required  Int
-         */
-        switchVideo: function($video, scenes, sceneCriteria, videoCriteria) {
-            var basename = scenes[sceneCriteria][videoCriteria];
-            var mp4Filename = 'video/' + basename + '.mp4';
-            var webmFilename = ''; // todo
-            $video.find('#mp4-source').attr('src', mp4Filename);
-            $video.find('#webm-source').attr('src', webmFilename); // todo
-            $video.load();
-        },
-
-        /**
-         * @param $video         Required  JQuery video element
-         * @param scenes         Required
-         * @param sceneCriteria  Optional  Int
-         */
-        switchToRandomVideo: function($video, scenes) {
-            if (arguments.length > 2) {
-                var sceneCriteria = parseInt(arguments[2]);
-            } else {
-                var sceneCriteria = this.getRandomInt(0, scenes.length);
-            }
-
-            if (sceneCriteria >= scenes.length) {
-                sceneCriteria = this.getRandomInt(0, scenes.length);
-            }
-
-            var videoCriteria = this.getRandomInt(0, scenes[sceneCriteria].length - 1);
-            this.switchVideo($video, scenes, sceneCriteria, videoCriteria);
         },
 
         /**
